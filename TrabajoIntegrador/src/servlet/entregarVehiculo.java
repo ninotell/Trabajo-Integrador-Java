de96222df31b6c1f2d50008c082d27d75092bd01
ninot <ninot@DESKTOP-3JMAQMI>
2021-07-12 19:08:52 -0300

    + errores

--- TrabajoIntegrador/src/servlet/entregarVehiculo.java
+++ TrabajoIntegrador/src/servlet/entregarVehiculo.java
@@ -27,17 +27,6 @@ public class entregarVehiculo extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-//	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	
     	
